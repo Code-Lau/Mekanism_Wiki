874abaccdcaf54a2443ce286eec2d0155b533676
Lauren Woods <laurenleighwoods@gmail.com>
2024-07-23 16:04:39 +0000

    Rename guides, add javascript file

---
+++ script.js
@@ -0,0 +1,38 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+    distance:'65px',
+    duration: 2600,
+    delay:450,
+    reset:true
+});
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:300, origin:'top'});
+sr.reveal('.navlist-2',{delay:400, origin:'left'});
+
+// Get the button:
+let mybutton = document.getElementById("arrow");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
