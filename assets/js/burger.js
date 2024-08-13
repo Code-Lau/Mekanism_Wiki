d6fc3ffb3b19aa97390ddafc6368a941ea873b20
Lauren Woods <laurenleighwoods@gmail.com>
2024-08-13 01:00:16 +0000

    Add function call

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -7,12 +7,14 @@ let header = document.querySelector('header');
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   header.classList.toggle('active');
-})
+  myFunction();
+});
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   header.classList.remove("active");
-})) 
+}));
+
 //Toggle between adding and removing the "responsive" class to nav-menu when the user clicks on the icon
 function myFunction() {
   var x = document.getElementById("mynav-menu");
